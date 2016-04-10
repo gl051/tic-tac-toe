71b5f8831d144d27bb71d38c0fe1d4697683b28d
Gianluca <gl051@biccari.com>
2016-04-09 17:16:20 -0700

    Fix verify on pos 5 of the grid. Also changed name convention for private memebers

--- grid.py
+++ grid.py
@@ -4,8 +4,8 @@
 
 class Grid(object):
     def __init__(self):
-        self.__board = [ ['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
-        self.__last_pos = 0
+        self._board = [ ['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
+        self._last_pos = 0
         self.empty_slots = [i for i in range(1,10)]
 
     def is_full(self):
@@ -18,7 +18,7 @@ class Grid(object):
         print ''
         for i in range(1, 10):
             (x, y) = self.__getCoordinates(i)
-            print '  {0}  |'.format(self.__board[x][y]),
+            print '  {0}  |'.format(self._board[x][y]),
             if i in [3, 6, 9]:
                 print ''
                 print '-------------------'
@@ -27,60 +27,60 @@ class Grid(object):
     def mark(self, pos, marker):
         (x, y) = self.__getCoordinates(pos)
         # check if the slot has been already marked
-        if self.__board[x][y] is not '-':
-            print 'Position {} already marked with {}'.format(pos, self.__board[x][y])
+        if self._board[x][y] is not '-':
+            print 'Position {} already marked with {}'.format(pos, self._board[x][y])
         else:
-            self.__board[x][y] = marker
-            self.__last_pos = pos
+            self._board[x][y] = marker
+            self._last_pos = pos
             self.empty_slots.remove(pos)
 
     def clear(self):
         for i in range(1, 10):
             self.mark(i, '-')
-        self.__last_pos = 0
+        self._last_pos = 0
         self.empty_slots = self.empty_slots = [i for i in range(1,10)]
 
     def is_winner(self):
-        if 0 < self.__last_pos < 10:
-            return self.__is_winner(self.__last_pos)
+        if 0 < self._last_pos < 10:
+            return self.__is_winner(self._last_pos)
         else:
             for i in range(1,10):
                 return self.__is_winner(i)
 
     def __is_winner(self, pos):
         if pos == 1:
-            return (self.__board[0][0] == self.__board[0][1] == self.__board[0][2] != '-' or
-                self.__board[0][0] == self.__board[1][0] == self.__board[2][0] != '-'  or
-                self.__board[0][0] == self.__board[1][1] == self.__board[2][2] != '-' )
+            return (self._board[0][0] == self._board[0][1] == self._board[0][2] != '-' or
+                self._board[0][0] == self._board[1][0] == self._board[2][0] != '-'  or
+                self._board[0][0] == self._board[1][1] == self._board[2][2] != '-' )
         elif pos == 2:
-            return (self.__board[0][1] == self.__board[1][1] == self.__board[2][1] != '-'  or
-                self.__board[0][0] == self.__board[0][1] == self.__board[0][2] != '-' )
+            return (self._board[0][1] == self._board[1][1] == self._board[2][1] != '-'  or
+                self._board[0][0] == self._board[0][1] == self._board[0][2] != '-' )
         elif pos == 3:
-            return (self.__board[0][0] == self.__board[0][1] == self.__board[0][2] != '-'  or
-                self.__board[0][2] == self.__board[1][2] == self.__board[2][2] != '-'  or
-                self.__board[0][2] == self.__board[1][1] == self.__board[2][0])
+            return (self._board[0][0] == self._board[0][1] == self._board[0][2] != '-'  or
+                self._board[0][2] == self._board[1][2] == self._board[2][2] != '-'  or
+                self._board[0][2] == self._board[1][1] == self._board[2][0])
         elif pos == 4:
-            return (self.__board[1][0] == self.__board[1][1] == self.__board[1][2] != '-'  or
-                self.__board[0][0] == self.__board[1][0] == self.__board[2][0] != '-' )
+            return (self._board[1][0] == self._board[1][1] == self._board[1][2] != '-'  or
+                self._board[0][0] == self._board[1][0] == self._board[2][0] != '-' )
         elif pos == 5:
-            return (self.__board[0][1] == self.__board[1][1] == self.__board[2][1] != '-'  or
-                self.__board[0][0] == self.__board[1][1] == self.__board[2][2] != '-'  or
-                self.__board[0][2] == self.__board[1][1] == self.__board[2][0] != '-'  or
-                self.__board[1][0] == self.__board[2][1] == self.__board[2][2] != '-' )
+            return (self._board[0][1] == self._board[1][1] == self._board[2][1] != '-'  or
+                self._board[0][0] == self._board[1][1] == self._board[2][2] != '-'  or
+                self._board[0][2] == self._board[1][1] == self._board[2][0] != '-'  or
+                self._board[1][0] == self._board[1][1] == self._board[1][2] != '-' )
         elif pos == 6:
-            return (self.__board[1][0] == self.__board[1][1] == self.__board[1][2] != '-'  or
-                self.__board[0][2] == self.__board[1][2] == self.__board[2][2] != '-' )
+            return (self._board[1][0] == self._board[1][1] == self._board[1][2] != '-'  or
+                self._board[0][2] == self._board[1][2] == self._board[2][2] != '-' )
         elif pos == 7:
-            return (self.__board[2][0] == self.__board[2][1] == self.__board[2][2] != '-'  or
-                self.__board[2][0] == self.__board[1][0] == self.__board[0][0] != '-'  or
-                self.__board[2][0] == self.__board[1][1] == self.__board[0][2] != '-' )
+            return (self._board[2][0] == self._board[2][1] == self._board[2][2] != '-'  or
+                self._board[2][0] == self._board[1][0] == self._board[0][0] != '-'  or
+                self._board[2][0] == self._board[1][1] == self._board[0][2] != '-' )
         elif pos == 8:
-            return (self.__board[2][1] == self.__board[1][1] == self.__board[0][1] != '-'  or
-                self.__board[2][0] == self.__board[2][1] == self.__board[2][2] != '-' )
+            return (self._board[2][1] == self._board[1][1] == self._board[0][1] != '-'  or
+                self._board[2][0] == self._board[2][1] == self._board[2][2] != '-' )
         elif pos == 9:
-            return (self.__board[2][2] == self.__board[2][1] == self.__board[2][0] != '-'  or
-                self.__board[2][2] == self.__board[1][2] == self.__board[0][2] != '-'  or
-                self.__board[2][2] == self.__board[1][1] == self.__board[0][0] != '-' )
+            return (self._board[2][2] == self._board[2][1] == self._board[2][0] != '-'  or
+                self._board[2][2] == self._board[1][2] == self._board[0][2] != '-'  or
+                self._board[2][2] == self._board[1][1] == self._board[0][0] != '-' )
         else:
             raise ValueError('Position in the grid must be between 1 and 9')
 
